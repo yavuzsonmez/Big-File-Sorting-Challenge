87af3eddb1d087446fbcb9a2da0d3f903aa94fde
Yavuz Sönmez <contact@yavuzsonmez.com>
2022-04-14 21:48:46 +0200

    some progress

--- src/SortFile.ts
+++ src/SortFile.ts
@@ -9,10 +9,10 @@ export default class SortFile {
 		{
 			if ((maxFileSizeBytes > Number.MAX_SAFE_INTEGER) || (maxFileSizeBytes <= 0))
 				throw new Error("Maximum file size in Byte is invalid: " + maxFileSizeBytes);
-			else if ((numberOfLinesPerSegment > (maxFileSizeBytes/lineSizeBytes/2)) || (numberOfLinesPerSegment <= 0))/*TODO*/
-				throw new Error("Number of lines per segment is invalid: " + numberOfLinesPerSegment);/*TODO*/
-			else if ((lineSizeBytes > Number.MAX_SAFE_INTEGER) || (lineSizeBytes <= 0))
-				throw new Error("Line size in Byte is invalid: " + lineSizeBytes);
+			else if ((2147483648 < (numberOfLinesPerSegment*lineSizeBytes)) || (numberOfLinesPerSegment <= 0))
+				throw new Error("Number of lines per segment and/or lineSizeBytes are invalid: " + numberOfLinesPerSegment + " " + lineSizeBytes);
+			else if (lineSizeBytes <= 0)
+				throw new Error("Line size in Byte cannot be equal or less than 0: " + lineSizeBytes);
 			this.maxFileSizeBytes = maxFileSizeBytes;
 			this.numberOfLinesPerSegment = numberOfLinesPerSegment;
 			this.lineSizeBytes = lineSizeBytes;
@@ -21,6 +21,8 @@ export default class SortFile {
 	public Sort(inFilename : string , outFilename : string ) : void {
 
 		try {
+			if (!fs.existsSync(inFilename))
+				throw new Error("inFilename is doesn't exist." );
 			const stats = fs.statSync(inFilename);
 			const data : string[] = [];
 			if (stats.size > this.maxFileSizeBytes)
@@ -29,29 +31,37 @@ export default class SortFile {
 				fs.open(inFilename, 'r', (err, fd) => {
 					if (err)
 						return console.error(err);
-					for(let i = 0; i < this.numberOfLinesPerSegment; i++)
+					for(let n = 0; n < (this.maxFileSizeBytes/this.lineSizeBytes/this.numberOfLinesPerSegment); n++)
 					{
-						const buffer: any = Buffer.alloc(this.lineSizeBytes);
-						fs.read(fd, buffer, 0, this.lineSizeBytes, null, (err, bytesRead, buffer) => {
+						for(let i = 0; i < this.numberOfLinesPerSegment; i++)
+						{
+							const buffer: any = Buffer.alloc(this.lineSizeBytes);
+							fs.read(fd, buffer, 0, this.lineSizeBytes, null, async (err, bytesRead, buffer) => {
+								if (err)
+									return console.error(err);
+								if (bytesRead > 0) {
+									data.push(buffer.toString());
+									console.log(data);
+								return (data)
+								}
+							});
+						}
+						fs.close(fd, (err) => {
 							if (err)
-								return console.error(err);
-							if (bytesRead > 0) {
-								data.push(buffer.toString());
-								console.log(data);
+							console.error(err);
+							else {
+								console.log("\n> File Closed");
 							}
+							data.sort();
+							console.log(data);
+							fs.writeFile(outFilename + n, data.join(''), 'ascii', (err) => {
+								if (err)
+									return console.error(err);
+							});
 						});
 					}
-					console.log(data);
-					fs.close(fd, (err) => {
-						if (err)
-							console.error(err);
-						else {
-							console.log("\n> File Closed");
-						}
-						//data.sort();
-					});
-			});
-		}
+				});
+			}
 		catch (err) {
 			console.error(err);
 		}
