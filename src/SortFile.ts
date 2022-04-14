ff7f9472148f584e96bdbfc3ef5fe3bf11c716df
Yavuz Sönmez <contact@yavuzsonmez.com>
2022-04-14 18:51:24 +0200

    dev

--- src/SortFile.ts
+++ src/SortFile.ts
@@ -13,39 +13,43 @@ export default class SortFile {
 				throw new Error("Number of lines per segment is invalid: " + numberOfLinesPerSegment);/*TODO*/
 			else if ((lineSizeBytes > Number.MAX_SAFE_INTEGER) || (lineSizeBytes <= 0))
 				throw new Error("Line size in Byte is invalid: " + lineSizeBytes);
-			console.log("Object constructed");
+			this.maxFileSizeBytes = maxFileSizeBytes;
+			this.numberOfLinesPerSegment = numberOfLinesPerSegment;
+			this.lineSizeBytes = lineSizeBytes;
 		};
 
-	public async Sort(inFilename : string , outFilename : string ) : Promise< void > {
+	public Sort(inFilename : string , outFilename : string ) : void {
 
 		try {
 			const stats = fs.statSync(inFilename);
-			let data : string[] = [];
+			const data : string[] = [];
 			if (stats.size > this.maxFileSizeBytes)
 				throw new Error("File size doesn't match with maxFileSizeBytes.");
+
 				fs.open(inFilename, 'r', (err, fd) => {
 					if (err)
 						return console.error(err);
 					for(let i = 0; i < this.numberOfLinesPerSegment; i++)
 					{
-						let buffer: any = Buffer.alloc(this.lineSizeBytes);
+						const buffer: any = Buffer.alloc(this.lineSizeBytes);
 						fs.read(fd, buffer, 0, this.lineSizeBytes, null, (err, bytesRead, buffer) => {
 							if (err)
 								return console.error(err);
 							if (bytesRead > 0) {
 								data.push(buffer.toString());
+								console.log(data);
 							}
-							//data.sort();
-							console.log(data);
 						});
 					}
+					console.log(data);
 					fs.close(fd, (err) => {
 						if (err)
 							console.error(err);
 						else {
 							console.log("\n> File Closed");
 						}
-				});
+						//data.sort();
+					});
 			});
 		}
 		catch (err) {
